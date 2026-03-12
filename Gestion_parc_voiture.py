cdeaaff0037179c5a5f61ae8f11a68917990eace
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-03-12 16:31:22 -0700

    implementation methode afficherInformation class voiture

--- Gestion_parc_voiture.py
+++ Gestion_parc_voiture.py
@@ -1,5 +1,18 @@
+class Voiture:
+    def __init__(self, matricule, annee, marque, kilometrage, chauffeur):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+        self.chauffeur = None
+    def afficherInformations(self):
+        print(f"Employe: {self.matricule} {self.annee} {self.marque} {self.kilometrage} ")
+        if self.chauffeur :
+            print(f"chauffeur:{self.chauffeur}")
+        else:
+            print(f"aucun chauffeur")
 class Employe:
-    def __init__(self, numeroPermis, nom, prenom):
+    def __init__(self, numeroPermis, nom, prenom,):
         self.numeropermis = numeroPermis
         self.nom = nom
         self.prenom = prenom
@@ -16,7 +29,7 @@ class Employe:
             print(f"l'employe a deja une voiture de service")
             return
         if Voiture.chauffeur != None:
-            Print(f"la voiture est deja attribue")
+           print(f"la voiture est deja attribue")
         self.voitureService = voitureService
         Voiture.chauffeur = self
         print(f"voiture de service attribue")
@@ -25,16 +38,13 @@ class Employe:
             print(f"l'employe n'a pas de voiture de service")
             return
         self.voitureService = voitureService
-        self.voitureService== None
-        Voiture.chauffeur = None
+        self.voitureService == None
+        self.VoitureVoiture.chauffeur = None
         print(f"la Voiture de service peut sortir")
-    class Voiture:
-        def __init__(self, matricule, annee, marque, kilometrage,chauffeur):
-            self.matricule = matricule
-            self.annee = annee
-            self.marque = marque
-            self.kilometrage = kilometrage
-            self.chauffeur = None
+
+
+
+
 
 
 
