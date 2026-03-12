8258f829afc5b9e374705669eb97281858a3c8ae
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-03-12 14:33:32 -0700

    implementation methode afficherInfos class Employe

--- Main.py
+++ Main.py
@@ -4,5 +4,15 @@ class Employe:
         self.nom = nom
         self.prenom = prenom
         self.voitureService=None
+    def afficherInformations(self):
+        print(f"Employe: {self.numeropermis} {self.nom} {self.prenom} {self.voitureService}")
+        if self.voitureService:
+            print(f"voiture service attribue")
+            self.voitureService.afficherInformations()
+        else:
+            print(f"voiture pas attribuee")
+
+
+
 
 
