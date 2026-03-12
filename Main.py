c14dca593ad5f0692f01791cf6d8d0920ca606f6
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-03-12 14:06:37 -0700

    creation class employe et ses attributs

---
+++ Main.py
@@ -0,0 +1,8 @@
+class Employe:
+    def __init__(self, numeroPermis, nom, prenom, voitureService):
+        self.numeropermis = numeroPermis
+        self.nom = nom
+        self.prenom = prenom
+        self.voitureService=None
+
+
