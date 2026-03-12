407fedda0949243539aa64101c988a41437d4bd9
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-03-12 15:25:30 -0700

    implementation methode affecter voitureServive class Employe

---
+++ Gestion_parc_voiture.py
@@ -0,0 +1,24 @@
+class Employe:
+    def __init__(self, numeroPermis, nom, prenom):
+        self.numeropermis = numeroPermis
+        self.nom = nom
+        self.prenom = prenom
+        self.voitureService=None
+    def afficherInformations(self):
+        print(f"Employe: {self.numeropermis} {self.nom} {self.prenom} {self.voitureService}")
+        if self.voitureService:
+            print(f"voiture service attribue")
+            self.voitureService.afficherInformations()
+        else:
+            print(f"voiture pas attribuee")
+    def affecterVoitureService(self, voitureService):
+        if self.voitureService != None:
+            print(f"l'employe a deja une voiture de service")
+            return
+        if Voiture.chauffeur != None:
+            Print(f"la voiture est deja attribue")
+        self.voitureService = voitureService
+        Voiture.chauffeur = self
+        print(f"voiture de service attribue")
+
+
--- Main.py
+++ Main.py
@@ -1,16 +1,4 @@
-class Employe:
-    def __init__(self, numeroPermis, nom, prenom, voitureService):
-        self.numeropermis = numeroPermis
-        self.nom = nom
-        self.prenom = prenom
-        self.voitureService=None
-    def afficherInformations(self):
-        print(f"Employe: {self.numeropermis} {self.nom} {self.prenom} {self.voitureService}")
-        if self.voitureService:
-            print(f"voiture service attribue")
-            self.voitureService.afficherInformations()
-        else:
-            print(f"voiture pas attribuee")
+
 
 
 
